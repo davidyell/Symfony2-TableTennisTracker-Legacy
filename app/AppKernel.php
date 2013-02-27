dae685359a9c86a5b6f7edaaa38de03bcc2185ef
David Yell <dyell@ukwebmedia.com>
2013-02-27 17:37:41 +0000

    Added new module.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new PingPong\Bundle\PlayerBundle\PingPongPlayerBundle(),
+            new Ornicar\GravatarBundle\OrnicarGravatarBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
