4a05f47b64178705f1059e18df25f25a22c633e0
David Yell <dyell@ukwebmedia.com>
2013-03-01 09:41:36 +0000

    Changed my mind and went back from YAML to Annotation routing. It just feels neater. Lame reason, but there you are.

--- src/PingPong/Bundle/PlayerBundle/Controller/DepartmentsController.php
+++ src/PingPong/Bundle/PlayerBundle/Controller/DepartmentsController.php
@@ -4,6 +4,7 @@ namespace PingPong\Bundle\PlayerBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use PingPong\Bundle\PlayerBundle\Entity\Department;
@@ -11,12 +12,15 @@ use PingPong\Bundle\PlayerBundle\Form\DepartmentType;
 
 /**
  * Department controller.
+ *
+ * @Route("/departments")
  */
 class DepartmentsController extends Controller
 {
     /**
      * Lists all Department entities.
      *
+     * @Route("/", name="departments")
      * @Template()
      */
     public function indexAction()
@@ -33,6 +37,7 @@ class DepartmentsController extends Controller
     /**
      * @param type $id
      * 
+     * @Route("/view/{id}", name="departments_view")
      * @Template()
      * 
      * @return type
@@ -52,6 +57,7 @@ class DepartmentsController extends Controller
      * @param type $id
      * @param \Symfony\Component\HttpFoundation\Request $request
      * 
+     * @Route("/edit/{id}", name="departments_edit")
      * @Template()
      * 
      * @return type
--- src/PingPong/Bundle/PlayerBundle/Controller/PlayersController.php
+++ src/PingPong/Bundle/PlayerBundle/Controller/PlayersController.php
@@ -3,18 +3,22 @@
 namespace PingPong\Bundle\PlayerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-
 use PingPong\Bundle\PlayerBundle\Form\PlayerType;
+
 use PingPong\Bundle\PlayerBundle\Entity\Player;
 
 /**
  * PlayersController
+ *
+ * @Route("/players")
  */
 class PlayersController extends Controller
 {
     /**
+     * @Route("/", name="players_index")
      * @Template()
      *
      * @return array Array of players
@@ -22,25 +26,26 @@ class PlayersController extends Controller
     public function indexAction()
     {
         $players = $this->getDoctrine()
-                           ->getRepository('PingPongPlayerBundle:Player')
-                           ->findAll();
+                        ->getRepository('PingPongPlayerBundle:Player')
+                        ->findAll();
 
         return array('players' => $players);
     }
 
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
-     * 
+     *
+     * @Route("/add", name="players_add")
      * @Template()
      *
      * @return mixed Array or Redirect
      */
-    public function addAction(Request $request)
+    public function addAction()
     {
         $form = $this->createForm(new PlayerType());
 
         if ($request->isMethod('POST')) {
-            $form->bind($request);
+            $form->bind($this->getRequest());
             $em = $this->getDoctrine()->getEntityManager();
             $em->persist($form->getData());
             $em->flush();
@@ -56,14 +61,14 @@ class PlayersController extends Controller
     }
 
     /**
-     * @param int $id The id of the record
-     * @param Request $request The form submission request
+     * @param int     $id      The id of the record
      *
+     * @Route("/edit/{id}", name="players_edit")
      * @Template()
      *
      * @return mixed An array or a redirect url
      */
-    public function editAction($id, Request $request)
+    public function editAction($id)
     {
         $player = $this->getDoctrine()
                        ->getRepository('PingPongPlayerBundle:Player')
@@ -72,7 +77,7 @@ class PlayersController extends Controller
         $form = $this->createForm(new PlayerType(), $player);
 
         if ($request->isMethod('POST')) {
-            $form->bind($request);
+            $form->bind($this->getRequest());
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
                 $em->persist($form->getData());
@@ -89,12 +94,13 @@ class PlayersController extends Controller
             'player' => $player
         );
     }
-    
+
     /**
      * @param int $id
-     * 
+     *
+     * @Route("/delete/{id}", name="players_delete")
      * @Template()
-     * 
+     *
      * @return string A redirection url
      */
     public function deleteAction($id)
@@ -102,11 +108,11 @@ class PlayersController extends Controller
         $player = $this->getDoctrine()
                        ->getRepository('PingPongPlayerBundle:Player')
                        ->findOneBy(array('id' => $id));
-        
+
         $em = $this->getDoctrine()->getEntityManager();
         $em->remove($player);
         $em->flush();
-        
+
         $this->get('session')->getFlashBag()->add('notice', 'Player deleted');
 
         return $this->redirect($this->generateUrl('players_index'));
